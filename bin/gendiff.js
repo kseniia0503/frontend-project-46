9a932f9717f6049d8e199cbfed5da1a8050d1488
Kseniia <kinozemtseva92@yandex.ru>
2025-07-09 22:03:57 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,9 +7,12 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version(version, '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'display help for command')
-  .parse(process.argv)
-
-  if (program.opts().help) {
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+console.log(`Comparing ${filepath1} and ${filepath2}`)
+  })
+  program.parse(process.argv)
+  if (process.argv.length < 3 || program.opts().help) {
   program.help()
 }
