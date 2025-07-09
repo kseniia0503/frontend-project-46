2a1f0293a2bdfae77df994d661ec48e1ae5b82fc
Kseniia <kinozemtseva92@yandex.ru>
2025-07-09 21:46:32 +0300

    step 1

---
+++ bin/gendiff.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const { program } = require('commander')
+const { version } = require('../package.json')
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version(version, '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'display help for command')
+  .parse(process.argv)
+
+  if (program.opts().help) {
+  program.help()
+}
